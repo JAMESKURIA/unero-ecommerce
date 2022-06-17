31a08377f1b9c9953498af58f7b8cbb8120b1f3d
nickjames <jameskuria04@gmail.com>
2022-06-17 16:52:06 +0300

    Updated Host Url again in config file

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
-import absoluteUrl from "next-absolute-url";
 const dev = process.env.NODE_ENV !== "production";
 
-const { origin } = absoluteUrl(req);
-
-export const server = dev ? "http://localhost:3000" : origin;
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://unero-ecommerce.vercel.app/";
